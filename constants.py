5c474b6df638aaea219ad8b3475f4acc232f7ff3
Zakir Mustafa Kemal Kalayci <118366212+ZakirMK@users.noreply.github.com>
2022-12-21 23:47:08 +0300

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,12 @@
+# Constant olması için büyük harflerle yazılır
+BASE_DOMAIN_URL = "https://www.saucedemo.com/"
+USERNAME_ID = "user-name"
+PASSWORD_ID = "password"
+LOGIN_ID = "login-button"
+LOGIN = "https://www.saucedemo.com/inventory.html"
+ERROR_LOGIN = '//*[@data-test="error"]'
+USER_MENU = "react-burger-menu-btn"
+INVENTORY_LIST = '//*[@class="inventory_item"]'
+PRODUCT1_ID = "add-to-cart-sauce-labs-backpack"
+PRODUCT_REMOVE_NAME = "remove-sauce-labs-backpack"
+BASKET_ITEMS = "shopping_cart_badge"
