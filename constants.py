b0b9eac8df8cd2e122e5be263a996dd947f926c9
Zakir Mustafa Kemal Kalayci <118366212+ZakirMK@users.noreply.github.com>
2022-12-22 08:50:34 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,12 +1,19 @@
 # Constant olması için büyük harflerle yazılır
 BASE_DOMAIN_URL = "https://www.saucedemo.com/"
 USERNAME_ID = "user-name"
+USERNAME = "standard_user"
 PASSWORD_ID = "password"
+PASSWORD = "secret_sauce"
 LOGIN_ID = "login-button"
+MENU_TEXT = "Open Menu"
+WRONG_PASSWORD = "wrong_password"
+ERROR_MENU_TEXT = "Epic sadface: Username and password do not match any user in this service"
 LOGIN = "https://www.saucedemo.com/inventory.html"
 ERROR_LOGIN = '//*[@data-test="error"]'
 USER_MENU = "react-burger-menu-btn"
 INVENTORY_LIST = '//*[@class="inventory_item"]'
 PRODUCT1_ID = "add-to-cart-sauce-labs-backpack"
 PRODUCT_REMOVE_NAME = "remove-sauce-labs-backpack"
+PRODUCT_REMOVE_TEXT = "REMOVE"
 BASKET_ITEMS = "shopping_cart_badge"
+BASKET_ITEMS_COUNT = "1"
