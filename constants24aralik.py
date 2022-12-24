dcf6cb5b8fc224a37c81e4c7cddad1d049ff14de
Zakir Mustafa Kemal Kalayci <118366212+ZakirMK@users.noreply.github.com>
2022-12-24 17:04:59 +0300

    Update constants24aralik.py

--- constants24aralik.py
+++ constants24aralik.py
@@ -13,34 +13,43 @@ LOCKED_ERROR_TEXT = "Epic sadface: Sorry, this user has been locked out."
 LOGIN = "https://www.saucedemo.com/inventory.html"
 ERROR_LOGIN = '//*[@data-test="error"]'
 USER_MENU = "react-burger-menu-btn"
+
 INVENTORY_ITEM = '//*[@class="inventory_item"]'
 INVENTORY_ITEM_NAME = '//*[@class="inventory_item_name"]'
+
 INV_PRODUCTNAME1 = "Sauce Labs Bike Light"
 INV_PRODUCTNAME2 = "Sauce Labs Bolt T-Shirt"
 INV_PRODUCTNAME3 = "Sauce Labs Onesie"
 INV_PRODUCTNAME4 = "Test.allTheThings() T-Shirt (Red)"
 INV_PRODUCTNAME5 = "Sauce Labs Backpack"
 INV_PRODUCTNAME6 = "Sauce Labs Fleece Jacket"
+
 INV_BIKELIGHT_ITEM_ID = "item_0_title_link"
 INV_BOLTTSHIRT_ITEM_ID = "item_1_title_link"
 INV_ONESIE_ITEM_ID = "item_2_title_link"
 INV_TESTALLTHINGSTSHIRT_ITEM_ID = "item_3_title_link"
 INV_BACKPACK_ITEM_ID = "item_4_title_link"
 INV_FLEECEJACKET_ITEM_ID = "item_5_title_link"
+
 PRODUCT_REMOVE_NAME = "remove-sauce-labs-backpack"
 PRODUCT_REMOVE_TEXT = "REMOVE"
+
 BASKET_ITEMS = "shopping_cart_badge"
 BASKET_ITEMS_COUNT = "1"
+
 SET_LIST = "*[data-test=\"product_sort_container\"]"
 SET_ZTOA = '//*[@value="za"]'
 SET_LOWHIGH = '//*[@value="lohi"]'
+
 ADD_CART_PRODUCTNAME1_ID = "add-to-cart-sauce-labs-bike-light"
 ADD_CART_PRODUCTNAME2_ID = "add-to-cart-sauce-labs-bolt-t-shirt"
 ADD_CART_PRODUCTNAME3_ID = "add-to-cart-sauce-labs-onesie"
 ADD_CART_PRODUCTNAME4_ID = "add-to-cart-test.allthethings()-t-shirt-(red)"
 ADD_CART_PRODUCTNAME5_ID = "add-to-cart-sauce-labs-backpack"
 ADD_CART_PRODUCTNAME6_ID = "add-to-cart-sauce-labs-fleece-jacket"
+
 REMOVE_CART_LABS_BACKPACK_NAME = "remove-sauce-labs-backpack"
+
 BASKET_BUTTON = '//*[@class="shopping_cart_link"]'
 BASKET_ITEM_CHECK = '//*[@class="shopping_cart_badge"]'
 BASKET_ITEM_REMOVE = "/html/body/div/div/div/div[2]/div/div[1]/div[3]"
